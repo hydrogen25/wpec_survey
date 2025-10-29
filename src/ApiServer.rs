41d8320fab5256c2907bd39268625096b1015f6a
hydrogenx <null>
2025-10-29 19:16:49 +0800

    init file structure

---
+++ src/ApiServer.rs
@@ -0,0 +1,6 @@
+use salvo::prelude::*;
+
+async fn start_api_server(){
+    let accepter = TcpListener::new("0.0.0.0:5800");
+    
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod ApiServer;
+
+fn main() {
+    println!("Hello, world!");
+}
